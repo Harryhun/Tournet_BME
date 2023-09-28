a66a85c611d346f8d20340cc36c9ef5b9a1b2391
Harryhun <bollerhenrik01@gmail.com>
2023-09-28 11:54:09 +0200

    splash screen

--- Android_part/app/src/main/java/hu/bme/aut/android/app_frontend/MainActivity.kt
+++ Android_part/app/src/main/java/hu/bme/aut/android/app_frontend/MainActivity.kt
@@ -2,7 +2,7 @@ package hu.bme.aut.android.app_frontend
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import hu.bme.aut.android.app_frontend.databinding.ActivityMainBinding
+//import hu.bme.aut.android.app_frontend.databinding.ActivityMainBinding
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
