896f95277868d45c496a8c3c1dfd90e8e7ff9f98
Harryhun <bollerhenrik01@gmail.com>
2023-09-28 11:43:26 +0200

    splash screen

--- Android_part/app/src/main/java/hu/bme/aut/android/app_frontend/MainActivity.kt
+++ Android_part/app/src/main/java/hu/bme/aut/android/app_frontend/MainActivity.kt
@@ -2,9 +2,11 @@ package hu.bme.aut.android.app_frontend
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
