2941000e4dda9ecb63e448f7fa9c54244bcb6742
Harryhun <bollerhenrik01@gmail.com>
2023-10-06 16:47:36 +0200

    doing bug

--- Android_part/app/build.gradle.kts
+++ Android_part/app/build.gradle.kts
@@ -40,7 +40,6 @@ android {
 
 dependencies {
     implementation("androidx.core:core-splashscreen:1.0.0")
-
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
@@ -51,7 +50,6 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     val nav_version = "2.5.3"
-
     implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
     implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
 }
--- Android_part/app/src/main/java/hu/bme/aut/android/app_frontend/MainActivity.kt
+++ Android_part/app/src/main/java/hu/bme/aut/android/app_frontend/MainActivity.kt
@@ -10,16 +10,20 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody.Companion.toRequestBody
 
+
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
+
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val client = OkHttpClient();
         val url = "http://10.0.2.2:3000";
+
+
 
         binding.startButton.setOnClickListener() {
             Thread{
