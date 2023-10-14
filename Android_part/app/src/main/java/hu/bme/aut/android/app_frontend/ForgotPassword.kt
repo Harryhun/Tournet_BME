ac9511d458158aa9e46039eb8928181dbd48bd1f
Harryhun <bollerhenrik01@gmail.com>
2023-10-14 21:15:32 +0200

    working login screen

--- Android_part/app/src/main/java/hu/bme/aut/android/app_frontend/ForgotPassword.kt
+++ Android_part/app/src/main/java/hu/bme/aut/android/app_frontend/ForgotPassword.kt
@@ -1,33 +1,25 @@
 package hu.bme.aut.android.app_frontend
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import hu.bme.aut.android.app_frontend.databinding.ActivityMainBinding
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import okhttp3.Credentials
-import okhttp3.MediaType.Companion.toMediaType
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.RequestBody.Companion.toRequestBody
 
 
-class MainActivity : AppCompatActivity() {
+class ForgotPassword : Fragment() {
+
+
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen()
-
         super.onCreate(savedInstanceState)
 
-
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-//
+
+        //setContentView(binding.root)
+
 //        val client = OkHttpClient();
 //        val url = "http://10.0.2.2:3000";
-//
-//
-//
-//
+
+
 //        binding.startButton.setOnClickListener() {
 //            Thread{
 //                val req = Request.Builder()
@@ -49,6 +41,6 @@ class MainActivity : AppCompatActivity() {
 //                binding.output.text = res.body?.string()
 //            }.start()
 //        }
-//
     }
+
 }
