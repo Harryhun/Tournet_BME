df0c32e4aa726a7dbfaf75ad9039085b770740ad
Harryhun <bollerhenrik01@gmail.com>
2023-10-14 21:48:00 +0200

    working on register and forgot password

--- Android_part/app/src/main/java/hu/bme/aut/android/app_frontend/ForgotPassword.kt
+++ Android_part/app/src/main/java/hu/bme/aut/android/app_frontend/ForgotPassword.kt
@@ -14,33 +14,7 @@ class ForgotPassword : Fragment() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
 
-        //setContentView(binding.root)
 
-//        val client = OkHttpClient();
-//        val url = "http://10.0.2.2:3000";
-
-
-//        binding.startButton.setOnClickListener() {
-//            Thread{
-//                val req = Request.Builder()
-//                    .url(url + "/sendData")
-//                    .addHeader("Authorization", Credentials.basic("user", "pass"))
-//                    .post(binding.input.text.toString().toRequestBody("text/plain; charset=utf-8".toMediaType()))
-//                    .build()
-//                client.newCall(req).execute()
-//            }.start()
-//        }
-//
-//        binding.exitButton.setOnClickListener() {
-//            Thread{
-//                val req = Request.Builder()
-//                    .url(url + "/requestData")
-//                    .addHeader("Authorization", Credentials.basic("user", "pass"))
-//                    .build()
-//                val res = client.newCall(req).execute()
-//                binding.output.text = res.body?.string()
-//            }.start()
-//        }
     }
 
 }
