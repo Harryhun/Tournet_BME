491e90baa01870e74a1e5cb40fc622d9789d8ac9
vSomaboi <s.volgyesi2002@gmail.com>
2023-11-24 13:17:24 +0100

    Connected StatMenu to backend

--- Android_part/app/src/main/java/hu/bme/aut/android/app_frontend/LoginFragment.kt
+++ Android_part/app/src/main/java/hu/bme/aut/android/app_frontend/LoginFragment.kt
@@ -53,8 +53,8 @@ class LoginFragment : Fragment() {
                  val result = connector.Login(binding.etUserName.text.toString(), binding.etPassword.text.toString())
                 when(result.getInt("status")){
                     1 -> {
-                        val action=LoginFragmentDirections.actionLoginFragmentToStartMenuFragment("Mukodik_safeargs")//just Test
-                        findNavController().navigate(action)
+                        //val action=LoginFragmentDirections.actionLoginFragmentToStartMenuFragment("Mukodik_safeargs")//just Test
+                        findNavController().navigate(R.id.action_loginFragment_to_startMenuFragment)
                     }
                     -1 -> {
                         Snackbar.make(it, "Connection problem", 5).show()
