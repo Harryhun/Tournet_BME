e4aab8c97aefd4a5df0a46679d0d3f0f1e6c3e31
kalocsaadi <kalocsaadam16@gmail.com>
2023-12-03 17:56:02 +0100

    server change 1

--- Backend_part/server.js
+++ Backend_part/server.js
@@ -10,7 +10,7 @@ console.log("Server started!")
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+    //res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
